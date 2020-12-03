ee78dbe46426d52f485698c39db9d8ab90c51a12
Rich Foley <richard.karl.foley@gmail.com>
2020-12-02 23:52:29 -0500

    Finished day2

--- 2020/day_02/solution.py
+++ 2020/day_02/solution.py
@@ -8,12 +8,27 @@ print(f'Inputs: {inputs}')
 
 valid_lines = []
 for line in inputs:
-    char_count = line[0].replace('-', ',')
+    char_count = line[0].split('-')
     match_char = line[1].strip(':')
     password = line[2]
     regex_patt = f'([{match_char}]{{1}}){{{char_count}}}'
-
-    if re.fullmatch(pattern=regex_patt, string=password):
+    matches = password.count(match_char)
+    if int(char_count[0]) <= matches <= int(char_count[1]):
         valid_lines.append([password, line])
 
 print(valid_lines)
+print('Part one:')
+print(f'{len(valid_lines)} of {len(inputs)} passwords are valid')
+
+valid_lines = []
+for line in inputs:
+    index1, index2 = line[0].split('-')
+    match_char = line[1].strip(':')
+    password = line[2]
+    if (password[int(index1)-1] == match_char and password[int(index2)-1] != match_char) \
+            or (password[int(index1)-1] != match_char and password[int(index2)-1] == match_char):
+        valid_lines.append([password, line])
+
+print(valid_lines)
+print('Part two:')
+print(f'{len(valid_lines)} of {len(inputs)} passwords are valid')
