5e92a0d4ea70607405102fad50a65b183029d81c
Rich Foley <richard.karl.foley@gmail.com>
2020-12-10 11:06:30 -0500

    day 10 test creds

--- 2020/day_10/solution.py
+++ 2020/day_10/solution.py
@@ -8,7 +8,6 @@ def get_file(input_file=r'd9input.txt'):
     return adapters
 
 
-
 # def check_num(current_idx):
 #     current_num_set = cypher[current_idx-preamble:current_idx]
 #     if not sum_valid(current_num_set, cypher[current_idx]):
